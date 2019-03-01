37f9a14f877157b780c1b8d14962e119b0a70d4b
Kartik Kulkarni <kartik.koolks@gmail.com>
2019-03-02 01:41:51 +0530

    Updates

---
+++ precache-manifest.304d9da3e21602bdbf6dde13a707eecc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86494e9c6d32fb67be2b",
+    "url": "/todo-list/static/js/runtime~main.86494e9c.js"
+  },
+  {
+    "revision": "b3611353b0a57212cd86",
+    "url": "/todo-list/static/js/main.b3611353.chunk.js"
+  },
+  {
+    "revision": "63458a11f8093835efd5",
+    "url": "/todo-list/static/js/2.63458a11.chunk.js"
+  },
+  {
+    "revision": "b3611353b0a57212cd86",
+    "url": "/todo-list/static/css/main.bdd7cdf5.chunk.css"
+  },
+  {
+    "revision": "e7fd0f683e46d87733bf12436c9106a7",
+    "url": "/todo-list/index.html"
+  }
+];
