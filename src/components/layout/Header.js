7a322a826e989d3327dc74188814554b9b4cbeb7
Kartik Kulkarni <kartik.koolks@gmail.com>
2019-03-02 01:32:43 +0530

    added: Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,22 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <div>
       <h1>TodoList</h1>
+      <Link style={linkStyle} to="/">
+        Home
+        <h3 style={{ marginLeft: "10" }}> </h3>
+      </Link>
+      <Link style={linkStyle} to="/about">
+        About
+      </Link>
     </div>
   );
 }
 
+const linkStyle = {
+  //   color: "#fff",
+  textDecoration: "none"
+};
 export default Header;
